3dc913a52768aa8c3adf533a3268a311053933ee
malikmajeed <malikmajeedzia@gmail.com>
2025-01-12 09:42:50 -0800

    Chatbot updated with react

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [Question, setQuestion] = useState('');
+  const [Answer, setAnswer] = useState('');
+
+  async function getAnswer() {
+
+    setAnswer('Loading...');
+    const response = await axios({
+      url:'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDn55YG4fUxjAr7nFiCT89tb5eCgnHeWG0',
+      method:'POST',
+        data:{contents: [{
+          parts:[{text: Question}]
+          }]}
+        
+   
+    });
+    console.log(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
+    setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
+  //  setAnswer(response.data.candidates[0].contents.parts[0].text);
+
+  }
+
+  return (
+    <>
+    <div>
+      <h1>My Ai bot</h1>
+      <textarea value={Question} onChange={(e)=>setQuestion(e.target.value)}></textarea>
+      <button onClick={getAnswer}>Get Answer</button>
+      <pre>{Answer}</pre>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
